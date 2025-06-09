e3d66b02bc89f46e7d99b17770067c2c28bfc306
Saurabh Mirajkar <mirajkar46@gmail.com>
2025-06-09 13:18:17 +0530

    updated fiel again

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            shrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
